452a6940466d2d43e23302c8ef05f70456f4ca3d
Aditya Jadhav <83344372+aditya-517@users.noreply.github.com>
2022-01-25 11:24:24 +0530

    Add files via upload

---
+++ AnalogClock/clock.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var hrotation = 30*h + m/2;
+    var mrotation = 6*m ;
+    var srotation = 6*s;
+
+    document.getElementById("hour").style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`
+    second.style.transform = `rotate(${srotation}deg)`
+}, 1000);
